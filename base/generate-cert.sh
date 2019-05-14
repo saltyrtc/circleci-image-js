d3d8ab34670c19c1e29c40ce4e8cac0ee188d44e
Lennart Grahl <lennart.grahl@gmail.com>
2019-05-14 14:47:48 +0200

    Bump to Ubuntu 18.xx and build FF 66 and 60 ESR

--- base/generate-cert.sh
+++ base/generate-cert.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
+set -Eeuo pipefail
+
+# Generate CA
+openssl genrsa -out /saltyrtc/certs/ca.key.pem 1024
 openssl req \
-    -newkey rsa:1024 \
-    -x509 \
-    -nodes \
-    -keyout /saltyrtc/certs/saltyrtc.key \
-    -new \
-    -out /saltyrtc/certs/saltyrtc.crt \
-    -subj /CN=localhost \
-    -reqexts SAN \
-    -extensions SAN \
-    -config <(cat /etc/ssl/openssl.cnf \
-        <(printf '[SAN]\nsubjectAltName=DNS:localhost')) \
-    -sha256 \
-    -days 1825
+    -config /saltyrtc/certs/openssl.cnf \
+    -key /saltyrtc/certs/ca.key.pem \
+    -new -x509 -days 365 -sha256 -extensions v3_ca \
+    -subj "/CN=SaltyRTC CA" \
+    -out /saltyrtc/certs/ca.cert.pem
+openssl x509 -in /saltyrtc/certs/ca.cert.pem -text -noout
+
+# Generate and sign localhost cert
+openssl genrsa -out /saltyrtc/certs/localhost.key.pem 1024
+openssl req \
+    -config /saltyrtc/certs/openssl.cnf \
+    -key /saltyrtc/certs/localhost.key.pem \
+    -new -sha256 \
+    -subj "/CN=localhost" \
+    -out /saltyrtc/certs/localhost.csr.pem
+openssl x509 \
+    -extfile /saltyrtc/certs/openssl.cnf \
+    -in /saltyrtc/certs/localhost.csr.pem \
+    -req -days 365 -sha256 -extensions server_cert \
+    -CA /saltyrtc/certs/ca.cert.pem \
+    -CAkey /saltyrtc/certs/ca.key.pem \
+    -CAcreateserial \
+    -out /saltyrtc/certs/localhost.cert.pem
+openssl x509 -in /saltyrtc/certs/localhost.cert.pem -text -noout
